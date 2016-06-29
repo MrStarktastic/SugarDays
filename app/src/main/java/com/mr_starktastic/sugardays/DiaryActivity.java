dcbebe2cde1b54099e1271676a0b20d43f9b48da
Ben Faingold <benfaingold@gmail.com>
2016-06-29 17:09:33 +0300

    Partially implemented collapsing calendarView

--- app/src/main/java/com/mr_starktastic/sugardays/DiaryActivity.java
+++ app/src/main/java/com/mr_starktastic/sugardays/DiaryActivity.java
@@ -1,41 +1,100 @@
 package com.mr_starktastic.sugardays;
 
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class DiaryActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
-    private static DrawerLayout drawer;
+        implements NavigationView.OnNavigationItemSelectedListener,
+        AppBarLayout.OnOffsetChangedListener {
+    private static final int DAYS_IN_WEEK = 7, ARROW_ANGLE = -180;
+    private static final Calendar CALENDAR = Calendar.getInstance();
+
+    private static DrawerLayout drawerLayout;
+    private static AppBarLayout appBar;
+    private static Toolbar toolbar;
+    private static MaterialCalendarView calendarView;
+    private static ViewGroup dropDownLayout;
+    private static TextView title, subtitle;
+    private static ImageView dropDownArrow;
+    private static FloatingActionButton fab;
+
+    private static boolean isCalendarShown = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary);
-        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        final NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        appBar = (AppBarLayout) drawerLayout.findViewById(R.id.app_bar);
+        appBar.addOnOffsetChangedListener(this);
+        toolbar = (Toolbar) appBar.findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        calendarView = (MaterialCalendarView) appBar.findViewById(R.id.calendar_view);
+        calendarView.setDynamicHeightEnabled(true);
+        calendarView.setTopbarVisible(false);
+        final DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        calendarView.setTileWidth((displayMetrics.widthPixels - getResources()
+                .getDimensionPixelSize(R.dimen.small_margin)) / DAYS_IN_WEEK);
+
+        dropDownLayout = (ViewGroup) toolbar.findViewById(R.id.dropdown_layout);
+        title = (TextView) dropDownLayout.findViewById(R.id.toolbar_title);
+        subtitle = (TextView) dropDownLayout.findViewById(R.id.toolbar_subtitle);
+        dropDownArrow = (ImageView) dropDownLayout.findViewById(R.id.dropdown_arrow);
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, MMM d", Locale.getDefault());
+        title.setText(dateFormat.format(CALENDAR.getTime()));
+        subtitle.setText(R.string.today);
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(v ->
                 Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show());
 
-        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        final ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
-                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
+        dropDownLayout.setOnClickListener(v -> toggleCalendarVisibility());
 
-        final NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        final ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    private void toggleCalendarVisibility() {
+        if (isCalendarShown) {
+            appBar.setExpanded(false, true);
+        } else {
+            appBar.setExpanded(true, true);
+        }
     }
 
     @Override
@@ -49,10 +108,29 @@ public class DiaryActivity extends AppCompatActivity
         }
     }
 
+    @SuppressLint("InflateParams")
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.diary, menu);
+        final View todayIconView = getLayoutInflater().inflate(R.layout.ic_today_num, null);
+
+        // Set today's number
+        ((TextView) todayIconView.findViewById(R.id.today_num_text))
+                .setText(String.valueOf(CALENDAR.get(Calendar.DAY_OF_MONTH)));
+
+        // Convert the layout into a bitmap for use as an icon
+        todayIconView.setDrawingCacheEnabled(true);
+        todayIconView.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
+                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+        todayIconView.layout(0, 0,
+                todayIconView.getMeasuredWidth(), todayIconView.getMeasuredHeight());
+        todayIconView.buildDrawingCache(true);
+        final Bitmap bitmap = Bitmap.createBitmap(todayIconView.getDrawingCache());
+        todayIconView.setDrawingCacheEnabled(false);
+        todayIconView.setVisibility(View.GONE);
+
+        // Set today's icon
+        menu.findItem(R.id.action_today).setIcon(new BitmapDrawable(getResources(), bitmap));
 
         return true;
     }
@@ -60,7 +138,7 @@ public class DiaryActivity extends AppCompatActivity
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.action_settings:
+            case R.id.action_today:
                 return true;
         }
 
@@ -87,8 +165,15 @@ public class DiaryActivity extends AppCompatActivity
 
         }
 
-        drawer.closeDrawer(GravityCompat.START);
+        drawerLayout.closeDrawer(GravityCompat.START);
 
-        return true;
+        return false;
+    }
+
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+        dropDownArrow.setRotation(
+                ARROW_ANGLE * (1 + (float) verticalOffset / calendarView.getHeight()));
+        isCalendarShown = verticalOffset == 0;
     }
 }
